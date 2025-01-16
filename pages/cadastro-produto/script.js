cdc45582461e86dbadaf214ec5996708a294af9f
dev-medeiros <med.rodrigo97@gmail.com>
2025-01-16 17:37:23 -0300

    Atualizando a função createProductCard para incluir ícones aos botões

--- pages/cadastro-produto/script.js
+++ pages/cadastro-produto/script.js
@@ -15,8 +15,8 @@ function createProductCard(name, description, price, categories) {
       <p><strong>Categorias:</strong> ${categories}</p>
     </div>
     <div>
-      <button class="edit-btn">Editar</button>
-      <button class="delete-btn">Apagar</button>
+      <button class="edit-btn"><i class="fas fa-edit"></i> Editar</button>
+      <button class="delete-btn"><i class="fas fa-trash-alt"></i> Apagar</button>
     </div>
   `;
 
@@ -24,15 +24,19 @@ function createProductCard(name, description, price, categories) {
   card.querySelector(".delete-btn").addEventListener("click", () => {
     productList.removeChild(card);
   });
+
   // Botão Editar
   card.querySelector(".edit-btn").addEventListener("click", () => {
+    // Preenche o formulário com os dados do card
     document.getElementById("name").value = name;
     document.getElementById("description").value = description;
     document.getElementById("price").value = price;
     document.getElementById("categories").value = categories;
+    // Remove o card da lista
     productList.removeChild(card);
   });
 
+  // Adiciona o card à lista de produtos
   productList.appendChild(card);
 }
 
@@ -40,6 +44,7 @@ function createProductCard(name, description, price, categories) {
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
+  // Obtendo valores dos campos do formulário
   const name = document.getElementById("name").value;
   const description = document.getElementById("description").value;
   const price = parseFloat(document.getElementById("price").value);
